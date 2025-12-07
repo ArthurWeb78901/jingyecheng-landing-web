52fc2a9bc749195b8a43decb9bb364f7066a46e8
Arthur Tsai <arthurlin78901@hotmail.com>
2025-12-06 18:53:02 -0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { ChatBubble } from "@/components/ChatBubble";
-import { ContactFormCn } from "@/components/ContactFormCN";
+import { ContactFormCn } from "@/components/ContactFormCn";
 
 type HomeGalleryItem = {
   id: number;
